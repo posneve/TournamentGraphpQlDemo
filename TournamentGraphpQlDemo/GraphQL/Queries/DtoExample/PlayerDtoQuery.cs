41213703b2116d34238ea8e5cffd9da8dce0db20
SNEVE Per Ola <per-ola.sneve@soprasteria.com>
2024-04-26 13:33:00 +0200

    Added som entity configurations

--- TournamentGraphpQlDemo/GraphQL/Queries/DtoExample/PlayerDtoQuery.cs
+++ TournamentGraphpQlDemo/GraphQL/Queries/DtoExample/PlayerDtoQuery.cs
@@ -58,6 +58,6 @@ public class PlayerExtensions
 public class PlayerDto
 {
     public Guid Id { get; set; } = Guid.NewGuid();
-    public string Name { get; set; }
-    public string NameAndId { get; set; }
+    public string Name { get; set; } = null!;
+    public string NameAndId { get; set; } = null!;
 }
--- TournamentGraphpQlDemo/Infrastructure/EntityFramework/TournamentContextConfiguration.cs
+++ TournamentGraphpQlDemo/Infrastructure/EntityFramework/TournamentContextConfiguration.cs
@@ -8,7 +8,11 @@ public class TournamentContextConfiguration :
     IEntityTypeConfiguration<Player>,
     IEntityTypeConfiguration<Match>,
     IEntityTypeConfiguration<MatchEvent>,
-    IEntityTypeConfiguration<Team>
+    IEntityTypeConfiguration<MatchGenericEvent>,
+    IEntityTypeConfiguration<MatchPlayerEvent>,
+    IEntityTypeConfiguration<Court>,
+    IEntityTypeConfiguration<Team>,
+    IEntityTypeConfiguration<Club>
 {
     public void Configure(EntityTypeBuilder<Player> builder)
     {
@@ -22,6 +26,7 @@ public class TournamentContextConfiguration :
             .HasMany(x => x.Clubs)
             .WithMany(x => x.Players);
         
+        builder.Property(x => x.Name).HasMaxLength(500);
     }
 
     public void Configure(EntityTypeBuilder<Team> builder)
@@ -29,6 +34,8 @@ public class TournamentContextConfiguration :
         builder.HasOne(x => x.Club)
             .WithMany(x => x.Teams)
             .HasForeignKey(x => x.ClubId);
+        
+        builder.Property(x => x.Name).HasMaxLength(500);
     }
     
     public void Configure(EntityTypeBuilder<MatchEvent> builder)
@@ -42,11 +49,22 @@ public class TournamentContextConfiguration :
             .HasValue<MatchGenericEvent>("MatchGenericEvent");
     }
 
+    public void Configure(EntityTypeBuilder<MatchGenericEvent> builder)
+    {
+        builder.Property(x => x.EventType)
+            .HasConversion<string>()
+            .HasMaxLength(100);
+    }
+    
     public void Configure(EntityTypeBuilder<MatchPlayerEvent> builder)
     {
         builder.HasOne(x => x.Player)
             .WithMany(x => x.MatchEvents)
             .HasForeignKey(x => x.PlayerId);
+        
+        builder.Property(x => x.EventType)
+            .HasConversion<string>()
+            .HasMaxLength(100);
     }
 
     public void Configure(EntityTypeBuilder<Match> builder)
@@ -62,5 +80,15 @@ public class TournamentContextConfiguration :
         builder.HasOne(x => x.Court)
             .WithMany(x => x.Matches)
             .HasForeignKey(x => x.CourtId);
+    }
+
+    public void Configure(EntityTypeBuilder<Court> builder)
+    {
+        builder.Property(x => x.Name).HasMaxLength(500);
+    }
+
+    public void Configure(EntityTypeBuilder<Club> builder)
+    {
+        builder.Property(x => x.Name).HasMaxLength(500);
     }
 }
